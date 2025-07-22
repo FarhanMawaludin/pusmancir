27d2835ebc55f637ec054fbed7d36c6ce98f03dd
FarhanMawaludin <farhanmawaludin7@gmail.com>
2025-07-22 20:28:00 +0700

    update

--- resources/views/admin/pengguna/index.blade.php
+++ resources/views/admin/pengguna/index.blade.php
@@ -106,10 +106,11 @@
                                 <a href="{{ asset('template_pengguna.xlsx') }}"
                                     class="flex items-center gap-2 px-3 py-1.5 text-sm bg-blue-600 text-white rounded hover:bg-blue-700 transition"
                                     download>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 16v-8m0 8l-3-3m3 3l3-3m6 8H6a2 2 0 01-2-2V6a2 2 0 012-2h6.586a2 2 0 011.414.586l5.414 5.414A2 2 0 0120 11.414V20a2 2 0 01-2 2z" />
+                                    <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M12 13V4M7 14H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2m-1-5-4 5-4-5m9 8h.01" />
                                     </svg>
                                     Download
                                 </a>
@@ -120,7 +121,8 @@
                                 class="space-y-4">
                                 @csrf
                                 <div>
-                                    <label for="excel_file" class="block mb-1 text-sm font-medium text-gray-900">Upload File
+                                    <label for="excel_file" class="block mb-1 text-sm font-medium text-gray-900">Upload
+                                        File
                                         Excel:</label>
                                     <label for="excel_file"
                                         class="flex items-center cursor-pointer rounded-md overflow-hidden border border-gray-300 bg-white">
